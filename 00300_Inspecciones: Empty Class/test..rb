a12b41369cca13eecca3c9fdeba313299d30f45f
franco@mumuki.org <franco@mumuki.org>
2016-11-22 02:09:15 -0500

    Mumuki Export on 2016-11-22 02:09:15 -0500

--- 00300_Inspecciones: Empty Class/test..rb
+++ 00300_Inspecciones: Empty Class/test..rb
@@ -28,7 +28,7 @@ describe "Inspections" do
      expect(inspector.empty_class? Foo).to be true
     end
 
-    it "una clase con un solo método no es empty class" do
+    it "una clase con un solo metodo no es empty class" do
      expect(inspector.empty_class? FooBar).to be false
     end
   end
